e60e7ed93243e27141329a83eb287c4273cf6cb9
eddo4life <theoboaz@gmail.com>
2023-12-28 12:11:07 -0500

    database scripts

--- README.txt
+++ README.txt
@@ -8,13 +8,12 @@
 | 33900 | THEORK    | Annold Casimir    |
 +-------+-----------+-------------------+
 
-# Configurez les paramètres d'accès à la base de données (hôte, utilisateur, mot de passe et nom de la base de données) dans le fichier 'database_credentials.json' situé à la racine du projet sous 'ubuntu'.
-
+Configurez les paramètres d'accès à la base de données (hôte, utilisateur, mot de passe et nom de la base de données) dans le fichier 'database_credentials.json' situé à la racine du projet sous 'ubuntu'.
 -- Ces informations sont vérifiées dans le fichier 'connection.py' situé à : ubuntu\database. [à analyser en cas de problème d'authentification]
 
 NB : Le fichier 'database_credentials.json' doit impérativement conserver ses propriétés (nom, format -> JSON), et toute modification des clés entraînera une exception !
- 
 
+Le script complet de la base de données se trouvent dans le dossier 'scripts' situé à la racine du projet sous 'ubuntu'.
 
 
 
---
+++ scripts/xparyaj_pariage.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: xparyaj
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pariage`
+--
+
+DROP TABLE IF EXISTS `pariage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pariage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_compte` varchar(255) NOT NULL,
+  `id_match` varchar(255) NOT NULL,
+  `date_pariage` date NOT NULL,
+  `score_prevu` varchar(5) NOT NULL,
+  `montant` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pariage`
+--
+
+LOCK TABLES `pariage` WRITE;
+/*!40000 ALTER TABLE `pariage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pariage` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-28 11:44:49
